ace53fe4b9f034a0cf9639a24336ad8274866fdd
Sarath <135338134+SarathA9@users.noreply.github.com>
2024-02-10 21:36:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-const text="META WORLD!";
-let deley=120;
-let direction=1;
-let index=0;
-
-function typetext(){
-    const typeelement=document.getElementById('typewriting');
-
-    if(direction==1 && index< text.length)
-    {
-        typeelement.textContent+=text.charAt(index);
-        index++;
-    }
-    else if(direction==-1 && index>=0)
-    {
-        typeelement.textContent=text.substring(0,index);
-        index--;
-    }
-    else{
-        direction*=-1;
-    }
-
-}
-setInterval(typetext,deley);
-typetext();
+window.addEventListener('load', function(){
+    document.getElementById('loading-overlay').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+  });

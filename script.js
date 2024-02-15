8dfbcf16db98efdab831f5bf2f56f2cc1d376a25
Sarath <135338134+SarathA9@users.noreply.github.com>
2024-02-15 22:13:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ window.addEventListener('load', function(){
     document.getElementById('loading-overlay').style.display = 'none';
     document.getElementById('content').style.display = 'block'; 
   });
+ 
